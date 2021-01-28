6f99cd5fe4ecb65b5b510709e6bf1af2530c2b08
lomabrdo <romain.lombardo69960r@gmail.com>
2021-01-28 16:28:15 +0100

    Début du site

--- forumIE/src/app/app-routing.module.ts
+++ forumIE/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ErrorComponent } from './error/error.component';
+import { TipsComponent } from './tips/tips.component';
+import { NewsComponent } from './news/news.component';
+import { ForumComponent } from './forum/forum.component';
+import { CookComponent } from './cook/cook.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path : 'news', component: NewsComponent},
+  {path : 'forum',  component : ForumComponent},
+  {path : 'accueil',  component: AccueilComponent},
+  {path : 'tips',  component: TipsComponent},
+  {path : 'cook',  component: CookComponent},
+  {path : '', component: AccueilComponent },
+  {path: 'not-found', component: ErrorComponent },
+  {path: '**', redirectTo: 'not-found' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- forumIE/src/app/app.module.ts
+++ forumIE/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ErrorComponent } from './error/error.component';
+import { TipsComponent } from './tips/tips.component';
+import { NewsComponent } from './news/news.component';
+import { ForumComponent } from './forum/forum.component';
+import { CookComponent } from './cook/cook.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    ErrorComponent,
+    TipsComponent,
+    NewsComponent,
+    ForumComponent,
+    CookComponent
   ],
   imports: [
     BrowserModule,
